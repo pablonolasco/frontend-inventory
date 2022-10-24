e22ae32f73177477fef191c3d8fd621c1c879e7c
pablo.nolasco <pablo.nolasco@live.com.mx>
2022-10-24 11:46:31 -0500

    Feat: add metodos del componente producto

--- src/app/modules/producto/components/producto/producto.component.ts
+++ src/app/modules/producto/components/producto/producto.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ProductoService } from 'src/app/modules/shared/services/producto.service';
 
 @Component({
@@ -8,9 +10,35 @@ import { ProductoService } from 'src/app/modules/shared/services/producto.servic
 })
 export class ProductoComponent implements OnInit {
 
+  displayComuns:string[]=[''];
+  dataSourceProductos= new MatTableDataSource<ProductoComponent>();
+  @ViewChild(MatPaginator)
+  paginador!:MatPaginator
   constructor(private productoService: ProductoService) { }
 
   ngOnInit(): void {
   }
+
+  //#region modal
+  abrirModalProducto(){
+
+  }
+  //#endregion
+
+  //#region metodos
+
+
+
+  buscarProducto(producto:any){
+
+  }
+  editarProducto(producto:any){
+
+  }
+
+  eliminarProducto(id:number){
+
+  }
+  //#endregion
 
 }
---
+++ src/interface/ProductoElement.ts
@@ -0,0 +1,8 @@
+export interface ProductoElements{
+  id:number;
+  nombre: string;
+  precio: number;
+  cantidad: number;
+  categoria: any;
+  picture: any;
+}
